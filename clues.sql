a254a5d64be4ca9e652559f7f96ecd9f283dfc4a
2000Bel <belvelizm@gmail.com>
2025-03-31 17:03:21 -0400

    I found Carmen Sandiego

--- clues.sql
+++ clues.sql
@@ -20,21 +20,21 @@ SELECT countries.name FROM countries JOIN countrylanguages ON countries.code = c
 
 SELECT name FROM cities WHERE countrycode = (SELECT code FROM countries WHERE name = 'San Marino') AND name != 'San Marino';
 
----Answer: Roma Milano
+---Answer:  Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-SELECT name, countrycode FROM cities WHERE name LIKE 'Mil%' AND countrycode IN (SELECT code FROM countries WHERE continent = 'South America'); ;
+SELECT name, countrycode FROM cities WHERE name LIKE 'Serr%' AND countrycode IN (SELECT code FROM countries WHERE continent = 'South America'); ;
 
----Aswer:  Milagro | ECU
+---Aswer:  Serra | BRA
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
-SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE name = 'Ecuador');
+SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE name = 'Brazil');
 
----Answer: Quito
+---Answer: Brasília
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -51,4 +51,4 @@ SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE name = '
 
 SELECT name, countrycode FROM cities WHERE population = 91084;
 
----Answer: 
+---Answer:  Santa Monica | USA
